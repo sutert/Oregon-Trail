afedc83c6957fb7499f66997c123dfc1bd61918b
Suter <sutert@amazon.com>
2019-10-29 14:12:23 -0400

    end class

--- oregon_trail.js
+++ oregon_trail.js
@@ -3,20 +3,37 @@ function Traveler(name) {
     this.food = 1;
     this.isHealthy = true;
 }
-let TavelerAna = new Traveler("Ana")
-//console.log(TavelerAna)
 
 function Wagon(capacity) {
     this.capacity = capacity;
     this.passengers = [];
 }
 
-   //Increase the traveler's food by 2.
+//hunt: Increase the traveler's food by 2.
 
-Traveler.prototype.hunt() = function() {
-        return this.food + 2;
-        console.log("get more food")
+Traveler.prototype.hunt() = function () {
+    this.food = this.food + 2;
+    console.log(Traveler + " get more food");
+}
+
+//eat: Consumes 1 unit of the traveler's food. 
+//If the traveler doesn't have any food to eat, the traveler is no longer healthy.
+
+Traveler.prototype.eat() = function () {
+    if (this.food >= 1) {
+        this.food = this.food - 1;
+
+    } else {
+        console.log(Traveler + " is now hungry (sick)");
+    }
 };
-console.log(TavelerAna)
-TavelerAna.hunt()
-console.log(TavelerAna)
+//available seat: Return the number of empty seats, determined by the 
+//capacity set when the wagon was created, compared to the number of 
+//passengers currently on board.
+
+Wagon.prototype.getAvailableSeatCount() = function () {
+    for (let i = 0; index < array.length; index++) {
+        const element = array[index];
+        
+    }
+};
